18478ac531cd03968f569892d5cb7b14a9ab726e
yanhy2000 <yanhy2000@qq.com>
2024-12-17 10:12:58 +0800

    物品监听

--- ImageGallery_MCMod/screenshot_uploader/src/main/java/top/yanhy/Screenshot_uploader.java
+++ ImageGallery_MCMod/screenshot_uploader/src/main/java/top/yanhy/Screenshot_uploader.java
@@ -1,24 +1,34 @@
 package top.yanhy;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 
+
+import net.minecraft.util.ActionResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Screenshot_uploader implements ModInitializer {
 	public static final String MOD_ID = "screenshot_uploader";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		LOGGER.info("mod initialized!");
 
-		LOGGER.info("Hello Fabric world!");
+		// 注册监听玩家使用物品事件
+		UseItemCallback.EVENT.register((player, world, hand) -> {
+			ItemStack itemStack = player.getStackInHand(hand);
+			// 判断物品是否为望远镜并重命名为相机
+			if (itemStack.getItem() == Items.SPYGLASS && itemStack.getName().getString().contains("相机")) {
+				LOGGER.info("玩家 {} 使用了望远镜-相机，已拦截。", player.getName().getString());
+				return ActionResult.FAIL;
+			}
+			LOGGER.info("玩家 {} 在世界 {} 使用了物品 {}。", player.getName().getString(), world.getRegistryKey().getValue().toString(),itemStack.getName().getString());
+
+			return ActionResult.PASS;
+		});
 	}
 }
---
+++ ImageGallery_MCMod/screenshot_uploader/src/main/java/top/yanhy/mixin/TestEventMixin.java
@@ -0,0 +1,20 @@
+package top.yanhy.mixin;
+
+import net.minecraft.server.MinecraftServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftServer.class)
+public class TestEventMixin {
+	@Inject(at = @At("HEAD"), method = "loadWorld")
+	private void init(CallbackInfo info) {
+		// This code is injected into the start of MinecraftServer.loadWorld()V
+		final String MOD_ID = "screenshot_uploader";
+		final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+		LOGGER.info("Screenshot Uploader is now enabled when the world loaded!");
+	}
+}
